cfd9fdbb78174c30355c82488970f97e73252200
prabhattopi <topi9864@gmail.com>
2025-01-23 11:52:00 +0530

    the file is save with dokcer config

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL });
 
 export const createUser = (username: string) =>
   API.post('/user', { username });
